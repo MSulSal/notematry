b76835c4b5678fb866c3984f5a2e8708e32b8bc6
MSulSal <msulsaleem@gmail.com>
2026-01-22 13:16:56 -0600

    notes flat list

--- app/notes/index.tsx
+++ app/notes/index.tsx
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, Text, View } from "react-native";
 
 export default function NoteScreen() {
+  const [notes, setNotes] = useState([
+    { id: "1", text: "Note One" },
+    { id: "2", text: "Note One" },
+    { id: "3", text: "Note One" },
+  ]);
+
   return (
     <View>
-      <Text>Notes</Text>
+      <FlatList
+        data={notes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.text}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
