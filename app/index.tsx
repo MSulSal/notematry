73b9a452e732be4aceedb52433562887a4104745
MSulSal <msulsaleem@gmail.com>
2026-01-22 13:09:53 -0600

    home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export default function Index() {
+export default function HomeScreen() {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>Welcome to NoteMatry</Text>
+      <TouchableOpacity onPress={() => router.push("./notes")}>
+        <Text>Get Started</Text>
+      </TouchableOpacity>
     </View>
   );
 }
